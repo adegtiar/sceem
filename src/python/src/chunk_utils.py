bf8b7214ccad9c60a9b44d72be8cd1466b11cb3b
Alex Degtiar <alex.degtiar@gmail.com>
2012-04-27 01:15:01 -0700

    Implementing the base ExecutorWrapper and SchedulerWrapper

--- src/python/src/chunk_utils.py
+++ src/python/src/chunk_utils.py
@@ -1,13 +1,74 @@
 import mesos
 
-class TaskNode(object)
+
+class TaskNode(object):
     pass
+
 
 class TaskTable(object):
     pass
 
+
 class ExecutorWrapper(mesos.Executor):
-    pass
+    """
+    Delegates calls to the underlying executor.
+    """
+
+    def __init__(self, executor):
+        self.executor = executor
+
+    def registered(self, driver, executorInfo, frameworkInfo, slaveInfo):
+        self.executor.registered(driver, executorInfo, frameworkInfo, slaveInfo)
+
+    def reregistered(self, driver, slaveInfo):
+        self.executor.reregistered(driver, slaveInfo)
+
+    def disconnected(self, driver):
+        self.executor.disconnected(driver)
+
+    def launchTask(self, driver, task):
+        self.executor.launchTask(driver, task)
+
+    def killTask(self, driver, taskId):
+        self.executor.killTask(driver, taskId)
+
+    def frameworkMessage(self, driver, message):
+        self.executor.frameworkMessage(driver, message)
+
+    def shutdown(self, driver):
+        self.executor.shutdown(driver)
+
 
 class SchedulerWrapper(mesos.Scheduler):
-    pass
+    """
+    Delegates calls to the underlying scheduler.
+    """
+    def __init__(self, scheduler):
+        self.scheduler = scheduler
+
+    def registered(self, driver, frameworkId, masterInfo):
+        self.scheduler.registered(driver, frameworkId, masterInfo)
+
+    def reregistered(self, driver, masterInfo):
+        self.scheduler.reregistered(driver, masterInfo)
+
+    def disconnected(self, driver):
+        self.scheduler.disconnected(driver)
+
+    def resourceOffers(self, driver, offers):
+        self.scheduler.resourceOffers(driver, offers)
+
+    def offerRescinded(self, driver, offerId):
+        self.scheduler.offerRescinded(driver, offerId)
+
+    def statusUpdate(self, driver, status):
+        self.scheduler.statusUpdate(driver, status)
+
+    def frameworkMessage(self, driver, message):
+        self.scheduler.frameworkMessage(driver, message)
+
+    def slaveLost(self, driver, slaveId):
+        self.scheduler.slaveLost(driver, slaveId)
+
+    def executorLost(self, driver, executorId, slaveId, status):
+        self.scheduler.executorLost(driver, executorId, slaveId, status)
