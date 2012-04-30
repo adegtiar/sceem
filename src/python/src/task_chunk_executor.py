d31a19ce9216c30c87fffceff0191baf3eb9e634
Alex Degtiar <alex.degtiar@gmail.com>
2012-04-29 21:21:29 -0700

    Trailing whitespace

--- src/python/src/task_chunk_executor.py
+++ src/python/src/task_chunk_executor.py
@@ -127,7 +127,7 @@ class TaskChunkExecutorDriver(chunk_utils.ExecutorDriverWrapper):
 
         if pending_tasks.isSubTask(update.task_id):
             updateMessage = chunk_utils.SubTaskUpdateMessage(update)
-            chunk_utils.ExecutorDriverWrapper.sendFrameworkMessage(self, 
+            chunk_utils.ExecutorDriverWrapper.sendFrameworkMessage(self,
                     updateMessage.toString())
             if chunk_utils.isTerminalUpdate(update):
                 parent = pending_tasks.getParent(update.task_id)
