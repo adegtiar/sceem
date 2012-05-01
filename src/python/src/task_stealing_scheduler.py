6ccbb7486ec821a3f6f8d8003abea7739eb7d536
Alex Degtiar <alex.degtiar@gmail.com>
2012-05-01 02:06:02 -0700

    Adding some stub implementations to task_stealing_scheduler

--- src/python/src/task_stealing_scheduler.py
+++ src/python/src/task_stealing_scheduler.py
@@ -8,3 +8,47 @@ class TaskStealingScheduler(chunk_utils.TaskChunkScheduler):
 
     def __init__(self, scheduler):
         chunk_utils.SchedulerWrapper.__init__(self, scheduler)
+
+    def resourceOffers(self, driver, offers):
+        pass
+
+    def resourceOffersStealing(self, driver, offers):
+        pass
+
+    def selectTasksToSteal(self, driver, offers, pending_tasks):
+        pass
+
+    def stealSubtasks(self, tasks):
+        pass
+
+    def frameworkMessage(self, executor_id, slave_id, driver, data):
+        pass
+
+
+def TaskStealingSchedulerDriver(chunk_utils.TaskChunkSchedulerDriver):
+
+    def updateTasks(tasks):
+        #Have underlying subtasks inherit from TaskChunk
+        for task in tasks:
+            for subTask in subTaskIterator(task):
+                subTask.executor.MergeFrom(task.executor)
+                task.slave_id.value = task.slave_id.value
+                if isTaskChunk(subTask):
+                    updateTasks((subTask,))
+
+    def updateResourceOffer(offerId, tasks):
+        consumedResources[offerId] += sum(task.resources for task in tasks)
+
+    # Updates the resources and calls the super method
+    def launchTasks(self, offerId, tasks, filters)
+        updateResourceOffer(offerId, tasks)
+        for task in tasks:
+            pendingTasks.addTask(task)
+        super.launchTasks(offerId, tasks filters)
+
+    def updateOffers(offers):
+        for offer in offers:
+            offer.resources -= consumedResources[offer.id]
+
+    def clearConsumedResources(offers_id):
+        del consumedResources[offer.id]
