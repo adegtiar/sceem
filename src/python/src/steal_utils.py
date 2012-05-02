434c4ee26e332871ab397a3fedc850b0e55869ad
Apoorva Sachdev <apoorva.1490@gmail.com>
2012-05-01 21:07:26 -0700

    steal_utils updated

--- src/python/src/steal_utils.py
+++ src/python/src/steal_utils.py
@@ -3,46 +3,95 @@ import chunk_utils
 import task_chunk_scheduler
 import mesos_pb2
 import pickle
+import heapq
 
 
 # TaskStates that indicate the task is done and can be cleaned up.
 TERMINAL_STATES = (mesos_pb2.TASK_FINISHED, mesos_pb2.TASK_FAILED,
             mesos_pb2.TASK_KILLED, mesos_pb2.TASK_LOST)
 
-class TaskCollator:
-    
-    def __init__(self, pending_tasks):
-        pass
+class PriortyQueue(object):
 
-    def getNext(self, isPrevAccepted):
-        """
-        Returns the next task and indicates if previous task was consumed
-
-        """
-        pass
-
-    def chooseNextDonor(self):
-        """
-        Returns the next task(chunk) which will be a donor for a task
-
-        """
-        pass
-
-
-class TaskConsumingOffers:
-    #Map<offerId, TaskChunk> stolenTasks
-    
-    def __init__(self, offers, numTasks, numCurrentOffers):
-        self.stolenTasks = {}
-        self.offers = offers
-        self.numTasks = numTasks
-
-    def isDone(self):
-        """
-        Checks if all offers filled
+  def __init__(self, initial=None, sort_key=lambda x: x, mapper=lambda x: x):
+    self.sort_key = sort_key
+    self.mapper = mapper
+		self.mapping = {}
+		self.__data = []
+		
+		if initial:
+			for item in initial:
+				self.push(item)
         
-        """
-        pass
+  def push(self, item):
+    item_key = self.mapper(item)
+    self.mapping[item_key] = item
+    heapq.heappush(self.__data, (self.sort_key(item), item_key))
+    
+  def pop(self):
+    item_key = heapq.heappop(self._data)[1]
+    item_value = self.mapping[item_key]
+    del self.mapping[item_key]
+    return item_value
+
+#The TaskQueue receives offers and returns tasks to give up to that offer.
+class TaskQueue:
+  def __init__(self, pending_tasks):
+    self.queue = PriorityQueue(pending_tasks, 
+                   sort_key = lambda task: -chunk_utils.numSubTasks(task),
+                   mapper = lambda offer: offer.id.value)
+
+    def fitsIn(task, offer):
+      """
+      Checks if task resources are less than Offer resources
+
+      """
+      taskRes = chunk_utils.getResourcesValue(task.resources)
+      offerRes = chunk_utils.getResourcesValue(task.offerRes)
+      for res_name, res_value in taskRes:
+        if res_value > offerRes[res_name]:
+          return False
+      return True
+
+    def stealHalfSubTasks(task):
+      """
+      Returns a list of stolenSubTasks and removes them from the task
+      
+      """
+      subTasks = [subTask for subTask in subTaskIterator(task)]
+      stolenTasks = subTasks[len(subTasks)/2:]
+      for stolenTask in stolenTasks:
+        chunk_utils.removeSubTask(task, stolenTask.task_id)
+        
+      return stolenTasks
+
+    def stealTasks(self, offer):
+      """
+      Give an offer to the queue. If accepted, it will return a
+      new task chunk containing the subTasks it stole. 
+      If rejected, returns None
+
+      """
+      popped_tasks = []
+      stolenTasksChunk = None
+      
+      while self.queue.hasNext():
+        task = self.queue.pop()
+        if (chunk_utils.numSubTasks(task) > 1 and self.fitsIn(task, offer)):
+          taskCopy = mesos_pb2.TaskInfo()
+          taskCopy.CopyFrom(task)
+          stolenTasks = stealHalfSubTasks(taskCopy)
+          stolenTasksChunk = chunk_utils.newTaskChunk(offer.slave_id,
+                                                  subTasks=stolenTasks)
+          stolenTasksChunk.name = "stolen chunk"
+          popped_tasks.append(taskCopy)
+          break
+        
+        popped_tasks.append(taskCopy)
+
+        for task in popped_tasks:
+          self.queue.push(task)
+
+      return stolenTasksChunk
 
 class TaskStealingSchedulerWrapper(task_chunk_scheduler.TaskChunkScheduler):
     """
