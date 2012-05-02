2a56d5771df0ca606df620f1915c88f836ad53a0
Alex Degtiar <alex.degtiar@gmail.com>
2012-05-02 01:49:39 -0700

    Adding a basic resourceOffer test

--- src/python/src/tests/task_stealing_scheduler_test.py
+++ src/python/src/tests/task_stealing_scheduler_test.py
@@ -6,6 +6,7 @@ sys.path.append(".")
 import stub_mesos
 sys.modules["mesos"] = stub_mesos
 
+import itertools
 import mesos
 import mesos_pb2
 import unittest
@@ -16,12 +17,23 @@ from task_stealing_scheduler import *
 class TestChunkScheduler(unittest.TestCase):
 
     def setUp(self):
-        self.mSchedulerDriver = MagicMock(spec=TaskStealingSchedulerDriver)
-        self.mScheduler = MagicMock(spec=mesos.Scheduler)
-        self.stealingScheduler = TaskStealingScheduler(self.mScheduler)
+        self.counter = itertools.count()
+
+        self.driver = MagicMock(spec=TaskStealingSchedulerDriver)
+        self.scheduler = MagicMock(spec=mesos.Scheduler)
+        self.stealingScheduler = TaskStealingScheduler(self.scheduler)
+
+    def nextId(self, offer_type):
+        return "{0}_id_{1}".format(offer_type, self.counter.next())
 
     def test_resourceOffers(self):
-        pass
+        offer = mesos_pb2.Offer()
+        offer.id.value = self.nextId("offer")
+        offer.framework_id.value = self.nextId("framework")
+        offer.slave_id.value = self.nextId("slave")
+        offer.hostname = self.nextId("local")
+
+        self.stealingScheduler.resourceOffers(self.driver, [offer])
 
 if __name__ == '__main__':
     unittest.main()
