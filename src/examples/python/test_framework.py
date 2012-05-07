0c35ea2d3b6643102d2a5af3d01df48f439d1f50
Alex Degtiar <alex.degtiar@gmail.com>
2012-05-06 20:59:12 -0700

    Making test framework parametrized by task size

--- src/examples/python/test_framework.py
+++ src/examples/python/test_framework.py
@@ -31,52 +31,54 @@ import task_stealing_scheduler
 
 
 # The number of slaves you intend to have in the cluster.
-NUM_SLAVES = 2
+DEFAULT_NUM_SLAVES = 1
 # The size of each task, in terms of the number of seconds it takes.
-TASK_TIME = 0.25
-# The total intended length of the simulation.
-SIMULATION_TIME = 32
+DEFAULT_TASK_TIME = 0.25
+# The total work per slave, in seconds.
+WORK_PER_SLAVE = 6
 # The distribution to use.
-DISTRIBUTION = task_utils.Distribution.NORMAL
+DEFAULT_DISTRIBUTION = task_utils.Distribution.UNIFORM
+# Default memory footprint of the executor.
+DEFAULT_TASK_MEM = 32
 
-tasks_per_slave = int(SIMULATION_TIME / TASK_TIME)
-num_total_tasks = tasks_per_slave * NUM_SLAVES
-TASK_IDS = set("Task_" + str(i) for i in range(num_total_tasks))
-
-TASK_MEM = 32
-task_cpus = None
-
-all_tasks = None
-
-#all_tasks = getTaskList(num_total_tasks, 32, 2, TASK_TIME, Distribution.NORMAL)
 
 class TestScheduler(mesos.Scheduler):
-  def __init__(self, executor):
+  def __init__(self, executor, num_slaves, task_time, num_total_tasks):
     self.executor = executor
     self.tasksLaunched = 0
     self.tasksFinished = 0
-    self.subTasksToKill = []
+
+    self.num_slaves = num_slaves
+    self.task_time = task_time
+    self.num_total_tasks = num_total_tasks
+    self.task_cpus = None
+    self.all_tasks = None
+    self.task_mem = DEFAULT_TASK_MEM
+    self.distribution = DEFAULT_DISTRIBUTION
+    self.task_ids = set("Task_" + str(i) for i in range(num_total_tasks))
+
+  def initializeTasks(self, offers):
+    for resource in offers[0].resources:
+        if resource.name == "cpus":
+            self.task_cpus = resource.scalar.value
+    self.all_tasks = task_utils.getTaskList(self.num_total_tasks, self.task_mem,
+            self.task_cpus, self.task_time, distribution=self.distribution)
 
   def registered(self, driver, frameworkId, masterInfo):
     print "Registered with framework ID %s" % frameworkId.value
 
   def resourceOffers(self, driver, offers):
     print "Got %d resource offers" % len(offers)
-    global all_tasks
 
     # Initialize the tasks.
-    if all_tasks is None:
-        for resource in offers[0].resources:
-            if resource.name == "cpus":
-                task_cpus = resource.scalar.value
-        all_tasks = task_utils.getTaskList(num_total_tasks, TASK_MEM, task_cpus, TASK_TIME,
-                distribution=DISTRIBUTION)
+    if self.all_tasks is None:
+        self.initializeTasks(offers)
 
     for offer in offers:
-      if all_tasks:
-        tasks_per_chunk = num_total_tasks / NUM_SLAVES
-        tasks = all_tasks[:tasks_per_chunk]
-        del all_tasks[:tasks_per_chunk]
+      if self.all_tasks:
+        tasks_per_chunk = self.num_total_tasks / self.num_slaves
+        tasks = self.all_tasks[:tasks_per_chunk]
+        del self.all_tasks[:tasks_per_chunk]
         self.tasksLaunched += tasks_per_chunk
 
         taskChunk = chunk_utils.newTaskChunk(offer.slave_id,
@@ -93,22 +95,19 @@ class TestScheduler(mesos.Scheduler):
 
   def statusUpdate(self, driver, update):
     print "Task %s is in state %d" % (update.task_id.value, update.state)
-    global num_total_tasks
     if update.state == mesos_pb2.TASK_FINISHED:
-      if update.task_id.value in TASK_IDS:
+      if update.task_id.value in self.task_ids:
+        self.task_ids.remove(update.task_id.value)
         self.tasksFinished += 1
-        if self.tasksFinished == num_total_tasks:
+        if self.tasksFinished == self.num_total_tasks:
           print "All tasks done, exiting"
           driver.stop()
       else:
         print "Task chunk finished: {0}".format(update.task_id.value)
 
 
-if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print "Usage: %s master" % sys.argv[0]
-    sys.exit(1)
-
+def runSimulation(master, num_slaves, task_time):
+  print "Starting simulation with task time {0}".format(task_time)
   executor = mesos_pb2.ExecutorInfo()
   executor.executor_id.value = "default"
   executor.command.value = os.path.abspath("./test-executor")
@@ -117,10 +116,38 @@ if __name__ == "__main__":
   framework.user = "" # Have Mesos fill in the current user.
   framework.name = "Test Framework (Python)"
 
+  tasks_per_slave = int(WORK_PER_SLAVE / task_time)
+  num_total_tasks = tasks_per_slave * num_slaves
+  scheduler = TestScheduler(executor, num_slaves, task_time, num_total_tasks)
 
+  start_time = time.time()
   driver = task_stealing_scheduler.TaskStealingSchedulerDriver(
-    TestScheduler(executor),
+    scheduler,
     framework,
-    sys.argv[1])
+    master)
+  success = driver.run() != mesos_pb2.DRIVER_STOPPED
+  elapsed_time = time.time() - start_time
+  print "Finished simulation with task time {0} after {1} seconds".format(
+          task_time, elapsed_time)
+  return success
 
-  sys.exit(0 if driver.run() == mesos_pb2.DRIVER_STOPPED else 1)
+
+if __name__ == "__main__":
+  if len(sys.argv) not in (3, 4):
+    print "Usage: %s master num_slaves, [task_sizes]" % sys.argv[0]
+    sys.exit(1)
+
+  master = sys.argv[1]
+  num_slaves = int(sys.argv[2])
+  if len(sys.argv) == 4:
+    task_times = [float(timeString) for timeString in sys.argv[3].split(',')]
+  else:
+    task_times = [DEFAULT_TASK_TIME]
+
+  for task_time in task_times:
+    success = runSimulation(master, num_slaves, task_time)
+    if not success:
+      print "Failed. Aborting."
+      sys.exit(1)
+
+  sys.exit(0)
